29302fb3628cd4ccbb83d8d708422d3d91899f98
kadirchan <kadircanbozkurt1905@gmail.com>
2024-08-23 22:00:07 +0300

    add MAC impl

--- contracts/src/DeviceIdentifierHelper.ts
+++ contracts/src/DeviceIdentifierHelper.ts
@@ -1,4 +1,4 @@
-import { CircuitString, Field } from 'o1js';
+import { CircuitString, Field, Struct } from 'o1js';
 import { uuidRegex } from './constants.js';
 
 export class UUID {
@@ -49,5 +49,52 @@ export class Serial {
   public static fromStringToCircuitString(serial: string): CircuitString {
     serial = serial.toUpperCase();
     return CircuitString.fromString(serial);
+  }
+}
+
+export class MacAddressField extends Struct({
+  ethernet: Field,
+  wifi: Field,
+}) {
+  constructor(macAddress: { ethernet: Field; wifi: Field }) {
+    macAddress.ethernet.assertNotEquals(Field(0));
+    macAddress.wifi.assertNotEquals(Field(0));
+    super(macAddress);
+  }
+}
+
+export class MacAddress {
+  constructor(public macAddress: string) {
+    if (!this.isValid()) {
+      throw new Error('Invalid MAC Address');
+    }
+    this.macAddress = this.macAddress
+      .replace(/:/g, '')
+      .replace(/-/g, '')
+      .toUpperCase();
+  }
+
+  isValid(): boolean {
+    const macAddressRegex = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+    return macAddressRegex.test(this.macAddress);
+  }
+
+  public toBigNumber(): string {
+    return BigInt('0x' + this.macAddress).toString(10);
+  }
+
+  public toField(): Field {
+    const bigint = this.toBigNumber();
+    return Field(bigint);
+  }
+
+  static fromStringArrayToMacAddressField(
+    macAddress: string[]
+  ): MacAddressField {
+    const [ethernet, wifi] = macAddress.map((mac) => {
+      const macAddress = new MacAddress(mac);
+      return macAddress.toField();
+    });
+    return new MacAddressField({ ethernet, wifi });
   }
 }
