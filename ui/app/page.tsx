0289ed9f7b7d04b9ddd11b2bafa4b0192b4261c0
kadirchan <kadircanbozkurt1905@gmail.com>
2024-09-07 20:51:43 +0300

    add page

---
+++ ui/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { useDeviceStore } from "@/lib/stores/deviceStore";
+import Store from "@/app/store/page";
+
+export default function Home() {
+    const gameName = useSearchParams()?.get("game");
+
+    const device = useSearchParams()?.get("device");
+
+    const router = useRouter();
+    const deviceStore = useDeviceStore();
+
+    useEffect(() => {
+        if (device || gameName) {
+            if (device) {
+                deviceStore.setDevice(JSON.parse(device));
+            }
+            if (gameName) router.push("/game-detail?game=" + gameName);
+        }
+    }, []);
+
+    return <Store />;
+}
