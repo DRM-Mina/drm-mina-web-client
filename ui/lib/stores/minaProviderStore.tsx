711c3b9b2e0abe96bf4959bc1a9f62dcc39b2e09
kadirchan <kadircanbozkurt1905@gmail.com>
2024-09-11 00:22:14 +0300

    user store and provider store ref

---
+++ ui/lib/stores/minaProviderStore.tsx
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+interface ProviderState {
+    isConnected: boolean;
+    walletInstalled: boolean;
+    userPublicKey?: string;
+    network?: string;
+
+    initializeWallet: () => Promise<void>;
+    connect: () => Promise<boolean>;
+    disconnect: () => void;
+}
+
+export const useProviderStore = create<ProviderState, [["zustand/immer", never]]>(
+    immer((set) => ({
+        isConnected: false,
+        walletInstalled: false,
+        userPublicKey: undefined,
+        network: undefined,
+
+        async initializeWallet() {
+            if (!window.mina) {
+                set((state) => {
+                    state.walletInstalled = false;
+                });
+                return;
+            }
+
+            const network = await window.mina.requestNetwork();
+            set((state) => {
+                state.network = network.chainId;
+            });
+            // const wallet = await window.mina.getAccounts();
+
+            // if (wallet[0]) {
+            //     set((state) => {
+            //         state.isConnected = true;
+            //         state.walletInstalled = true;
+            //         state.userPublicKey = wallet[0];
+            //     });
+            // }
+        },
+
+        async connect() {
+            if (!window.mina) {
+                return false;
+            }
+
+            const wallet = await window.mina.getAccounts();
+            if (wallet[0]) {
+                set((state) => {
+                    state.isConnected = true;
+                    state.walletInstalled = true;
+                    state.userPublicKey = wallet[0];
+                });
+                return true;
+            }
+
+            return false;
+        },
+
+        disconnect() {
+            set((state) => {
+                state.isConnected = false;
+                state.userPublicKey = undefined;
+            });
+        },
+    }))
+);
