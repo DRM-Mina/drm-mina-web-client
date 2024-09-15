07471a59b845f2fdb0f949fc8ca278bc2ac37309
kadirchan <kadircanbozkurt1905@gmail.com>
2024-09-15 23:44:27 +0300

    npm publish

--- contracts/src/lib/DeviceIdentifier.ts
+++ contracts/src/lib/DeviceIdentifier.ts
@@ -1,5 +1,10 @@
 import { CircuitString, Field, Poseidon, Struct } from 'o1js';
-import { MacAddressField } from './DeviceIdentifierHelper.js';
+import {
+  MacAddress,
+  MacAddressField,
+  Serial,
+  UUID,
+} from './DeviceIdentifierHelper.js';
 
 export interface RawIdentifiers {
   cpuId: string;
@@ -18,6 +23,24 @@ export class Identifiers extends Struct({
   macAddress: MacAddressField,
   diskSerial: CircuitString,
 }) {
+  constructor(
+    public cpuId: Field,
+    public systemSerial: CircuitString,
+    public systemUUID: CircuitString,
+    public baseboardSerial: CircuitString,
+    public macAddress: MacAddressField,
+    public diskSerial: CircuitString
+  ) {
+    super({
+      cpuId,
+      systemSerial,
+      systemUUID,
+      baseboardSerial,
+      macAddress,
+      diskSerial,
+    });
+  }
+
   toFields() {
     return [
       this.cpuId,
@@ -28,6 +51,28 @@ export class Identifiers extends Struct({
       this.macAddress.wifi.hash(),
       this.diskSerial.hash(),
     ];
+  }
+
+  static fromRaw(raw: RawIdentifiers): Identifiers {
+    const cpuId = Field.from(raw.cpuId);
+    const systemSerial = Serial.fromStringToCircuitString(raw.systemSerial);
+    const systemUUID = UUID.fromStringToCircuitString(raw.systemUUID);
+    const baseboardSerial = Serial.fromStringToCircuitString(
+      raw.baseboardSerial
+    );
+    const macAddress = MacAddress.fromStringArrayToMacAddressField(
+      raw.macAddress
+    );
+    const diskSerial = Serial.fromStringToCircuitString(raw.diskSerial);
+
+    return new Identifiers(
+      cpuId,
+      systemSerial,
+      systemUUID,
+      baseboardSerial,
+      macAddress,
+      diskSerial
+    );
   }
 
   hash() {
---
+++ contracts/src/lib/index.ts
@@ -0,0 +1,4 @@
+export * from './DeviceIdentifier.js';
+export * from './DeviceIdentifierHelper.js';
+export * from './DeviceIdentifierProof.js';
+export * from './constants.js';
