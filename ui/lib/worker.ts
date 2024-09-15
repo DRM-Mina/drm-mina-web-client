4c820e105017d04e1c20750916e9b29a1e60ceb5
kadirchan <kadircanbozkurt1905@gmail.com>
2024-09-15 23:44:05 +0300

    web worker impl

--- ui/lib/worker.ts
+++ ui/lib/worker.ts
@@ -1,61 +1,137 @@
 import "reflect-metadata";
+import { Identifiers } from "drm-mina-contracts/build/src/lib/DeviceIdentifier";
 import {
-  Identifiers,
-  DeviceIdentifier,
-  DeviceIdentifierProof,
-} from "drm-mina-chain/dist/src/index.js";
+    DeviceIdentifier,
+    DeviceIdentifierProof,
+} from "drm-mina-contracts/build/src/lib/DeviceIdentifierProof";
+import { GameToken } from "drm-mina-contracts/build/src/GameToken";
+import { fetchAccount, Mina, PublicKey } from "o1js";
+
+type Transaction = Awaited<ReturnType<typeof Mina.transaction>>;
 
 const state = {
-  status: "loading" as "loading" | "ready",
-  deviceIdentifierProgram: null as typeof DeviceIdentifier | null,
-  verificationKey: null as string | null,
+    status: "loading" as "loading" | "ready",
+    deviceIdentifierProgram: null as typeof DeviceIdentifier | null,
+    contracts: {
+        GameToken: {
+            contract: null as null | typeof GameToken,
+            zkapp: null as null | GameToken,
+        },
+    },
+    transaction: null as null | Transaction,
+    // verificationKey: null as string | null,
 };
+export type State = typeof state;
+
+export type ContractName = keyof State["contracts"];
 
 const functions = {
-  compileProgram: async (args: {}) => {
-    state.deviceIdentifierProgram = DeviceIdentifier;
-    const deviceIdentifierKey = await DeviceIdentifier.compile();
-    state.verificationKey = deviceIdentifierKey.verificationKey;
-  },
-  createDeviceIdentifierProof: async (args: {
-    rawIdentifiers: RawIdentifiers;
-  }) => {
-    if (!state.deviceIdentifierProgram) {
-      throw new Error("Program not compiled");
-    }
+    setActiveInstanceToDevnet: async (args: {}) => {
+        const Network = Mina.Network("https://api.minascan.io/node/devnet/v1/graphql");
+        console.log("Devnet network instance configured.");
+        Mina.setActiveInstance(Network);
+    },
+    loadContract: async ({ contractName }: { contractName: ContractName }) => {
+        if (!state.contracts[contractName])
+            throw new Error(`${contractName} contract is not defined`);
+        if (!state.contracts[contractName].contract) {
+            const contract = (await import(`drm-mina-contracts/build/src/${contractName}.js`))[
+                contractName
+            ];
+            if (!contract) {
+                throw new Error(`Could not load contract ${contractName} from the module`);
+            }
+            state.contracts[contractName].contract = contract;
+        }
+    },
+    compileContract: async ({ contractName }: { contractName: ContractName }) => {
+        const contract = state.contracts[contractName]?.contract;
+        if (!contract) throw new Error(`${contractName} contract is not loaded`);
+        await contract.compile();
+    },
+    loadAndCompileContract: async (args: { contractName: ContractName }) => {
+        await functions.loadContract(args);
+        await functions.compileContract(args);
+    },
+    fetchAccount: async (args: { publicKey58: string }) => {
+        const publicKey = PublicKey.fromBase58(args.publicKey58);
+        return await fetchAccount({ publicKey });
+    },
+    initZkappInstance: async ({
+        publicKey58,
+        contractName,
+    }: {
+        contractName: ContractName;
+        publicKey58: string;
+    }) => {
+        const contract = state.contracts[contractName]?.contract;
+        if (!contract) throw new Error(`${contractName} contract is not loaded`);
+        state.contracts[contractName].zkapp = new contract(PublicKey.fromBase58(publicKey58));
+    },
 
-    const identifiers = Identifiers.fromRaw(args.rawIdentifiers);
-    const proof: DeviceIdentifierProof =
-      await state.deviceIdentifierProgram.proofForDevice(identifiers);
-    return JSON.stringify(proof.toJSON(), null, 2);
-  },
+    getPrice: async (args: {}) => {
+        const currentPrice = await state.contracts["GameToken"].zkapp!.gamePrice.fetch();
+        const currentDiscount = await state.contracts["GameToken"].zkapp!.discount.fetch();
+        return JSON.stringify({
+            price: currentPrice?.toString(),
+            discount: currentDiscount?.toString(),
+        });
+    },
+    buyGame: async ({ recipient }: { recipient: string }) => {
+        const transaction = await Mina.transaction(async () => {
+            await state.contracts["GameToken"].zkapp!.mintGameToken(
+                PublicKey.fromBase58(recipient)
+            );
+        });
+        state.transaction = transaction;
+    },
+    proveUpdateTransaction: async (args: {}) => {
+        await state.transaction!.prove();
+    },
+    getTransactionJSON: async (args: {}) => {
+        return state.transaction!.toJSON();
+    },
+
+    compileProgram: async (args: {}) => {
+        state.deviceIdentifierProgram = DeviceIdentifier;
+        const deviceIdentifierKey = await DeviceIdentifier.compile();
+        // state.verificationKey = deviceIdentifierKey.verificationKey.data
+    },
+    createDeviceIdentifierProof: async (args: { rawIdentifiers: RawIdentifiers }) => {
+        if (!state.deviceIdentifierProgram) {
+            throw new Error("Program not compiled");
+        }
+
+        const identifiers = Identifiers.fromRaw(args.rawIdentifiers);
+        const proof: DeviceIdentifierProof = await state.deviceIdentifierProgram.proofForDevice(
+            identifiers
+        );
+        return JSON.stringify(proof.toJSON(), null, 2);
+    },
 };
 export type WorkerFunctions = keyof typeof functions;
 
 export type ZkappWorkerRequest = {
-  id: number;
-  fn: WorkerFunctions;
-  args: any;
+    id: number;
+    fn: WorkerFunctions;
+    args: any;
 };
 
 export type ZkappWorkerReponse = {
-  id: number;
-  data: any;
+    id: number;
+    data: any;
 };
 
 if (typeof window !== "undefined") {
-  addEventListener(
-    "message",
-    async (event: MessageEvent<ZkappWorkerRequest>) => {
-      const returnData = await functions[event.data.fn](event.data.args);
+    addEventListener("message", async (event: MessageEvent<ZkappWorkerRequest>) => {
+        const returnData = await functions[event.data.fn](event.data.args);
 
-      const message: ZkappWorkerReponse = {
-        id: event.data.id,
-        data: returnData,
-      };
-      postMessage(message);
-    },
-  );
+        const message: ZkappWorkerReponse = {
+            id: event.data.id,
+            data: returnData,
+        };
+        postMessage(message);
+    });
 }
 
 console.log("Web Worker Successfully Initialized.");
--- ui/lib/workerClient.ts
+++ ui/lib/workerClient.ts
@@ -1,56 +1,114 @@
+import { fetchAccount, PublicKey } from "o1js";
 import type {
-  ZkappWorkerRequest,
-  ZkappWorkerReponse,
-  WorkerFunctions,
+    ZkappWorkerRequest,
+    ZkappWorkerReponse,
+    WorkerFunctions,
+    ContractName,
 } from "./worker";
 
 export default class WorkerClient {
-  compileProgram() {
-    return this._call("compileProgram", {});
-  }
+    setActiveInstanceToDevnet() {
+        return this._call("setActiveInstanceToDevnet", {});
+    }
 
-  createDeviceIdentifierProof({
-    rawIdentifiers,
-  }: {
-    rawIdentifiers: RawIdentifiers;
-  }): Promise<any> {
-    return this._call("createDeviceIdentifierProof", {
-      rawIdentifiers,
-    }) as Promise<any>;
-  }
+    loadAndCompileContract(args: { contractName: ContractName }) {
+        return this._call("loadAndCompileContract", args);
+    }
 
-  worker: Worker;
+    loadContract(args: { contractName: ContractName }) {
+        return this._call("loadContract", args);
+    }
 
-  promises: {
-    [id: number]: { resolve: (res: any) => void; reject: (err: any) => void };
-  };
+    compileContract(args: { contractName: ContractName }) {
+        return this._call("compileContract", args);
+    }
 
-  nextId: number;
+    compileProgram() {
+        return this._call("compileProgram", {});
+    }
 
-  constructor() {
-    this.worker = new Worker(new URL("./worker.ts", import.meta.url));
-    this.promises = {};
-    this.nextId = 0;
+    fetchAccount({ publicKey }: { publicKey: PublicKey }): ReturnType<typeof fetchAccount> {
+        const result = this._call("fetchAccount", {
+            publicKey58: publicKey.toBase58(),
+        });
+        return result as ReturnType<typeof fetchAccount>;
+    }
 
-    this.worker.onmessage = (event: MessageEvent<ZkappWorkerReponse>) => {
-      this.promises[event.data.id].resolve(event.data.data);
-      delete this.promises[event.data.id];
+    initZkappInstance({
+        contractName,
+        publicKey,
+    }: {
+        contractName: ContractName;
+        publicKey: PublicKey;
+    }) {
+        return this._call("initZkappInstance", {
+            contractName,
+            publicKey58: publicKey.toBase58(),
+        });
+    }
+
+    async getPrice(): Promise<any> {
+        const result = await this._call("getPrice", {});
+        return JSON.parse(result as string);
+    }
+
+    buyGame({ recipient }: { recipient: string }) {
+        return this._call("buyGame", {
+            recipient,
+        });
+    }
+
+    proveUpdateTransaction() {
+        return this._call("proveUpdateTransaction", {});
+    }
+
+    async getTransactionJSON() {
+        const result = await this._call("getTransactionJSON", {});
+        return result;
+    }
+
+    createDeviceIdentifierProof({
+        rawIdentifiers,
+    }: {
+        rawIdentifiers: RawIdentifiers;
+    }): Promise<any> {
+        return this._call("createDeviceIdentifierProof", {
+            rawIdentifiers,
+        }) as Promise<any>;
+    }
+
+    worker: Worker;
+
+    promises: {
+        [id: number]: { resolve: (res: any) => void; reject: (err: any) => void };
     };
-  }
 
-  _call(fn: WorkerFunctions, args: any) {
-    return new Promise((resolve, reject) => {
-      this.promises[this.nextId] = { resolve, reject };
+    nextId: number;
 
-      const message: ZkappWorkerRequest = {
-        id: this.nextId,
-        fn,
-        args,
-      };
+    constructor() {
+        this.worker = new Worker(new URL("./worker.ts", import.meta.url));
+        this.promises = {};
+        this.nextId = 0;
 
-      this.worker.postMessage(message);
+        this.worker.onmessage = (event: MessageEvent<ZkappWorkerReponse>) => {
+            this.promises[event.data.id].resolve(event.data.data);
+            delete this.promises[event.data.id];
+        };
+    }
 
-      this.nextId++;
-    });
-  }
+    _call(fn: WorkerFunctions, args: any) {
+        return new Promise((resolve, reject) => {
+            this.promises[this.nextId] = { resolve, reject };
+
+            const message: ZkappWorkerRequest = {
+                id: this.nextId,
+                fn,
+                args,
+            };
+
+            this.worker.postMessage(message);
+
+            this.nextId++;
+        });
+    }
 }
