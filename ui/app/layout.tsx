4369014bc9a0889e2e6cd2deeb34e3e7074e3f30
kadirchan <kadircanbozkurt1905@gmail.com>
2024-11-11 14:14:21 +0300

    mobile web message

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -10,7 +10,7 @@ import { useWorkerStore } from "@/lib/stores/workerStore";
 import { useWalletStore } from "@/lib/stores/walletStore";
 import { useObserveGames } from "@/lib/stores/gameStore";
 import useHasMounted from "@/lib/customHooks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { useObserveUserLibraryRoutine } from "@/lib/stores/userStore";
 
@@ -27,21 +27,26 @@ export default function RootLayout({
     walletStore.observeWalletChange();
     const { toast } = useToast();
     const hasMounted = useHasMounted();
+    const [isMobile, setIsMobile] = useState(false);
     useEffect(() => {
-        (async () => {
-            if (workerStore.isLoading || workerStore.isReady) {
-                return;
-            }
-            toast({
-                title: "Web workers compiling contracts",
-                description: "Web workers are compiling contracts, please wait",
-            });
-            await workerStore.startWorker();
-            toast({
-                title: "Web workers ready",
-                description: "Web workers are ready",
-            });
-        })();
+        if (window.innerWidth <= 768) {
+            setIsMobile(true);
+        } else {
+            (async () => {
+                if (workerStore.isLoading || workerStore.isReady) {
+                    return;
+                }
+                toast({
+                    title: "Web workers compiling contracts",
+                    description: "Web workers are compiling contracts, please wait",
+                });
+                await workerStore.startWorker();
+                toast({
+                    title: "Web workers ready",
+                    description: "Web workers are ready",
+                });
+            })();
+        }
     }, [hasMounted]);
 
     useObserveUserLibraryRoutine();
@@ -59,19 +64,30 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    <div className="absolute inset-0 border-t">
-                        <div className="absolute inset-0 bg-background">
-                            <div className="grid grid-cols-6">
-                                <Sidebar className="sticky top-0 col-span-1" />
-                                <main className=" col-start-2 col-end-7 overflow-hidden">
-                                    <SearchBar />
-                                    {children}
-                                </main>
-                                <Footer className="col-start-2 col-end-7" />
-                                <Toaster />
+                    {!isMobile ? (
+                        <div className="absolute inset-0 border-t">
+                            <div className="absolute inset-0 bg-background">
+                                <div className="grid grid-cols-6">
+                                    <Sidebar className="sticky top-0 col-span-1" />
+                                    <main className=" col-start-2 col-end-7 overflow-hidden">
+                                        <SearchBar />
+                                        {children}
+                                    </main>
+                                    <Footer className="col-start-2 col-end-7" />
+                                    <Toaster />
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div className="flex flex-col items-center justify-center h-screen m-10">
+                            <p className="text-center text-lg font-semibold">
+                                DRM Mina is not available on mobile devices yet.
+                            </p>
+                            <p className="text-center text-lg font-semibold">
+                                Please use a desktop browser.
+                            </p>
+                        </div>
+                    )}
                 </ThemeProvider>
             </body>
         </html>
