a40589bc43731536fa34f53356343ab2939b6810
kadirchan <kadircanbozkurt1905@gmail.com>
2024-11-12 02:40:42 +0300

    color fix

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -17,79 +17,79 @@ import { useObserveUserLibraryRoutine } from "@/lib/stores/userStore";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const workerStore = useWorkerStore();
-    const walletStore = useWalletStore();
-    useObserveGames();
-    walletStore.observeWalletChange();
-    const { toast } = useToast();
-    const hasMounted = useHasMounted();
-    const [isMobile, setIsMobile] = useState(false);
-    useEffect(() => {
-        if (window.innerWidth <= 768) {
-            setIsMobile(true);
-        } else {
-            (async () => {
-                if (workerStore.isLoading || workerStore.isReady) {
-                    return;
-                }
-                toast({
-                    title: "Web workers compiling contracts",
-                    description: "Web workers are compiling contracts, please wait",
-                });
-                await workerStore.startWorker();
-                toast({
-                    title: "Web workers ready",
-                    description: "Web workers are ready",
-                });
-            })();
+  const workerStore = useWorkerStore();
+  const walletStore = useWalletStore();
+  useObserveGames();
+  walletStore.observeWalletChange();
+  const { toast } = useToast();
+  const hasMounted = useHasMounted();
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    } else {
+      (async () => {
+        if (workerStore.isLoading || workerStore.isReady) {
+          return;
         }
-    }, [hasMounted]);
+        toast({
+          title: "Web workers compiling contracts",
+          description: "Web workers are compiling contracts, please wait",
+        });
+        await workerStore.startWorker();
+        toast({
+          title: "Web workers ready",
+          description: "Web workers are ready",
+        });
+      })();
+    }
+  }, [hasMounted]);
 
-    useObserveUserLibraryRoutine();
+  useObserveUserLibraryRoutine();
 
-    return (
-        <html lang="en">
-            <head>
-                <title>DRM Mina</title>
-                <meta charSet="utf-8" />
-            </head>
-            <body className={inter.className}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    {!isMobile ? (
-                        <div className="absolute inset-0 border-t">
-                            <div className="absolute inset-0 bg-background">
-                                <div className="grid grid-cols-6">
-                                    <Sidebar className="sticky top-0 col-span-1" />
-                                    <main className=" col-start-2 col-end-7 overflow-hidden">
-                                        <SearchBar />
-                                        {children}
-                                    </main>
-                                    <Footer className="col-start-2 col-end-7" />
-                                    <Toaster />
-                                </div>
-                            </div>
-                        </div>
-                    ) : (
-                        <div className="flex flex-col items-center justify-center h-screen m-10">
-                            <p className="text-center text-lg font-semibold">
-                                DRM Mina is not available on mobile devices yet.
-                            </p>
-                            <p className="text-center text-lg font-semibold">
-                                Please use a desktop browser.
-                            </p>
-                        </div>
-                    )}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <title>DRM Mina</title>
+        <meta charSet="utf-8" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {!isMobile ? (
+            <div className="absolute inset-0 border-t">
+              <div className="absolute inset-0 bg-background">
+                <div className="grid grid-cols-6">
+                  <Sidebar className="sticky top-0 col-span-1" />
+                  <main className=" col-start-2 col-end-7 overflow-hidden">
+                    <SearchBar />
+                    {children}
+                  </main>
+                  <Footer className="col-start-2 col-end-7" />
+                  <Toaster />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-screen m-10">
+              <p className="text-center text-lg font-semibold">
+                DRM Mina is not available on mobile devices yet.
+              </p>
+              <p className="text-center text-lg font-semibold">
+                Please use a desktop browser.
+              </p>
+            </div>
+          )}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
