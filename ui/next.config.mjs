00b1427fa62f6ca280896b0711623c40f39ad653
kadirchan <kadircanbozkurt1905@gmail.com>
2024-11-14 18:12:41 +0300

    disable minification

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,5 +1,5 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,8 +12,9 @@ const nextConfig = {
     if (!isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        o1js: path.resolve(__dirname, 'node_modules/o1js/dist/web/index.js'),
+        o1js: path.resolve(__dirname, "node_modules/o1js/dist/web/index.js"),
       };
+      config.optimization.minimize = false;
     }
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
@@ -23,15 +24,15 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
           },
           {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
           },
         ],
       },
